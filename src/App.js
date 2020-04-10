a26ba1e2ba801a799b556581581f673cc77923c7
juanseromo12 <juanseromo12@gmail.com>
2020-04-10 17:34:27 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, {useState} from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import AuthState from './context/authentication/authState';
-import Login from './components/login';
+// import Login from './components/login';
 import Header from './components/header';
 import Footer from './components/footer';
-import Salas from './components/salas';
-import Ranking from './components/ranking';
+// import Salas from './components/salas';
+// import Ranking from './components/ranking';
 import RegisterUser from './components/registerUser';
 import tokenAuth from './config/tokenAuth';
-import WrapperInRoom from './components/inRoom';
-import Payments from './components/payments';
-import RutaPrivada from './components/rutas/RutaPrivada';
+// import WrapperInRoom from './components/inRoom';
+// import Payments from './components/payments';
+// import RutaPrivada from './components/rutas/RutaPrivada';
 // import Session from './components/HOC-Session'
 // import {withRouter} from 'react-router-dom'
 
@@ -26,6 +26,12 @@ function App() {
   const logout = () => {
     setUser('')
   }
+
+  const Example = () => (
+    <div>
+      Test
+    </div>
+  )
   return (
     <div className="">
       <AuthState>
@@ -33,12 +39,14 @@ function App() {
           <Header user={user} logout={logout}  />
           <div style={{minHeight: '90vh'}}>
             <Switch>
-              <Route exact path="/" render={() => <Login refetchUser={refetchUser} />}/>
+              <Route exact path="/" render={() => <Example/>}/>
+              {/* <Route exact path="/" render={() => <Login refetchUser={refetchUser} />}/>
+              {/* <Route exact path="/" render={() => <Login refetchUser={refetchUser} />}/>
               <RutaPrivada exact path="/rooms" component={Salas}/>
               <RutaPrivada exact path="/in-room" component={WrapperInRoom}/>
               <RutaPrivada exact path="/payments" component={Payments}/>
               <Route exact path="/ranking" render={ () => <Ranking/> } />
-              <Route exact path="/register" render={ () => <RegisterUser/> } />
+              <Route exact path="/register" render={ () => <RegisterUser/> } /> */}
             </Switch>
           </div>
           <Footer/>
